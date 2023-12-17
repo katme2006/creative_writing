8de109cc9e2c0779869fbd416e911cc714be2c44
KATE_FISCHER <KATME2006@GMAIL.COM>
2023-12-16 22:10:51 -0600

    hero in progress

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import PromptComponent from '../components/PromptGenerator';
-import ViewAllResponses from '../components/View_All_Responses';
-import RecentCollectionsList from '../components/Display_Collections';
-import '../styles/Home.css';
+import React from "react";
+import PromptComponent from "../components/PromptGenerator";
+import ViewAllResponses from "../components/View_All_Responses";
+import RecentCollectionsList from "../components/Display_Collections";
+import handwrittenVideo from '../assets/handwritten.mp4';
+import mobileImage from '../assets/HandWriting.svg'; // Ensure this path is correct
+import "../styles/Home.css";
 
 const HomePage = ({ userToken }) => {
+  // User is not logged in
   if (!userToken) {
-    // User is not logged in
     return (
       <div>
-        <div className='hero'>
-          <div className='left-hero'>
+        <div className="hero">
+          <div className="left-hero">
             <h1>Write Now.</h1>
             <p>Welcome to Write Now! Generate a writing prompt to get started.</p>
           </div>
@@ -23,12 +25,22 @@ const HomePage = ({ userToken }) => {
   // User is logged in
   return (
     <div>
-      <div className='hero'>
-        <div className='left-hero'>
-          <h1>Write Now.</h1>
-          <p>This page will include a basic generator with no additional features, and a product introduction/explanation</p>
+      <div className="hero">
+        <div className="left-hero">
+          {/* Video will only display for desktop sizes */}
+          <video autoPlay muted  className="background-video desktop-only">
+            <source src={handwrittenVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          {/* Image will only display for mobile sizes */}
+          <img src={mobileImage} alt="Handwritten Background" className="mobile-only" />
+          
         </div>
+        <div className="right-hero">
+        <h1>Write Now.</h1>
+          <p>This page will include a basic generator with no additional features, and a product introduction/explanation</p>
         <PromptComponent userToken={userToken} />
+        </div>
       </div>
       <RecentCollectionsList userToken={userToken} />
       <ViewAllResponses userToken={userToken} />
